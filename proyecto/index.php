f546a72f02923a685ae876a25251869dfbbac309
Mariysabel <mariysabelc@gmail.com>
2026-05-07 22:38:48 -0600

    correcion en index 2

--- proyecto/index.php
+++ proyecto/index.php
@@ -2,28 +2,6 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Trupper tso</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="relative min-h-screen flex items-center justify-center">
-
-    <div class="absolute inset-0 bg-cover bg-center"
-         style="background-image: url('resources/img/fondo.jpg');">
-    </div>
-
-    <div class="absolute inset-0 bg-gradient-to-r from-orange-500/80 to-black/90"></div>
-
-    <div class="relative text-center text-white px-6">
-        
-        <h1 class="text-4xl md:text-5xl font-bold mb-4">
-            TRUPPER
-        </h1>
-
-        <p class="text-lg md:text-xl mb-8 opacity-90 text-bold">
-            Misión<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trupper TSO</title>
     <script src="https://cdn.tailwindcss.com"></script>
