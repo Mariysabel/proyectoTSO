2ffc1006535d7f28b7fd9102880f54bf33ab174c
Mariysabel <mariysabelc@gmail.com>
2026-06-03 09:37:15 -0600

    tercera oportunidad TSO MEPR nue

--- proyecto/index.php
+++ proyecto/index.php
@@ -21,7 +21,7 @@
         <div class="text-center mb-12">
 
             <h1 class="text-5xl md:text-7xl font-extrabold text-orange-500 tracking-wide">
-                TRUPPER PRUEBA NUMERO DOS
+                TRUPPER PRUEBA NUMERO DOSqs
             </h1>
 
             <div class="w-28 h-1 bg-orange-500 mx-auto mt-4 mb-5 rounded-full"></div>
